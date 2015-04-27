2b837d97891d7499096553bbcf7f67e58262347b
rdanieli <rafaelldanieli@gmail.com>
2015-04-27 11:21:17 -0300

    ws  test

---
+++ Project/src/main/java/com/eng/univates/checkrest/MessageRestService.java
@@ -0,0 +1,18 @@
+package com.eng.univates.checkrest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/message")
+public class MessageRestService {
+	
+	@GET
+	@Path("/{param}")
+	public Response testRest( @PathParam("param") String msg ) {
+		String result = "WebService Restful: " + msg;
+		return Response.status(200).entity(result).build();
+	}
+	
+}
